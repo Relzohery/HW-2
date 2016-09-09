5d214e5889a8d01000a344fd2fddca2f0e386aed
Relzohery <Rabab.elzohery@gmail.com>
2016-09-08 22:18:42 -0500

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script gives a count of the total no.of files and subdirectories  in the current directory
+
+
+  echo " Number of files and  subdirectories = $( ls -Al | grep -c '-')" 
+
