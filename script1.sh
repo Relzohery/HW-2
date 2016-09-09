68e39680d1c763cdc83d47d8a5d3248b332777d1
Rabab Elzohery <Rabab. elzohery@gmail.com>
2016-09-08 23:36:10 -0500

    spelling correction

--- script1.sh
+++ script1.sh
@@ -10,9 +10,9 @@ echo " Enter value of temperature in fahrenheit "
 
   read F
 
-  echo " Temerature in fahrenheit equals  "  
+  echo " Temperature in fahrenheit equals  "  
    echo " scale=3 ; (((( $F-32 ))*5/9 )) " |bc
- echo " Temerature in kelvin  equals  "
+ echo " Temperature in kelvin  equals  "
    echo " scale=3 ; (((( $F+459.67 ))*5/9 )) " |bc
   
   
